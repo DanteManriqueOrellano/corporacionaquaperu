8df30d83a3464188441388a2ee67eccf64bc7d13
DanteManriqueOrellano <relaxurbano@me.com>
2020-05-11 11:23:20 -0500

    joder

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProyectoGeneralidadContainerComponent } from '../proyecto-generalidad/proyecto-generalidad-container/proyecto-generalidad-container.component';
 //import { DepartamentoComponent } from '../modules/ubigeo/departamento/departamento.component';
 const routes: Routes = [
   /*{path:'ubigeo',loadChildren:  '../modules/ubigeo/ubigeo.module#UbigeoModule'},*/
   /* {path:'ubigeo',loadChildren: () => import('../modules/ubigeo/ubigeo.module').then(m => m.UbigeoModule)},
   {path:'bienvenido',loadChildren: () => import('../modules/bienvenido/bienvenido.module').then(m => m.BienvenidoModule)},
   {path:'referencia', loadChildren:()=> import('../modules/generalidadesproyecto/generalidadesproyecto.module').then(m=>m.GeneralidadesproyectoModule)}, */
+  {path:'generalidad',component:ProyectoGeneralidadContainerComponent}
  
 
  
