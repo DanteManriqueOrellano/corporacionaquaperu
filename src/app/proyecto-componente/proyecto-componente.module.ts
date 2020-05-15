c1482182ac3bf6144330054f269a9313b80c7e1c
DanteManriqueOrellano <relaxurbano@me.com>
2020-05-15 02:31:25 -0500

    joder

--- src/app/proyecto-componente/proyecto-componente.module.ts
+++ src/app/proyecto-componente/proyecto-componente.module.ts
@@ -7,15 +7,16 @@ import { ComponenteRootComponent } from './componente-container/componente-root/
 import { CaptacionComponent } from './componente-container/componente-root/captacion/captacion.component';
 import { ReservorioComponent } from './componente-container/componente-root/reservorio/reservorio.component';
 import { TecnologiaExcretaComponent } from './componente-container/componente-root/tecnologia-excreta/tecnologia-excreta.component';
+
+import { LineaConduccionComponent } from './componente-container/componente-root/red-tuberia/linea-conduccion/linea-conduccion.component';
+import { LineaAduccionComponent } from './componente-container/componente-root/red-tuberia/linea-aduccion/linea-aduccion.component';
+import { RedDistribucionComponent } from './componente-container/componente-root/red-tuberia/red-distribucion/red-distribucion.component';
+import { ConexionDomiciliariaComponent } from './componente-container/componente-root/red-tuberia/conexion-domiciliaria/conexion-domiciliaria.component';
 import { RedTuberiaComponent } from './componente-container/componente-root/red-tuberia/red-tuberia.component';
-import { LineaConduccionComponent } from './componente-container/componente-root/red_tuberia/linea-conduccion/linea-conduccion.component';
-import { LineaAduccionComponent } from './componente-container/componente-root/red_tuberia/linea-aduccion/linea-aduccion.component';
-import { RedDistribucionComponent } from './componente-container/componente-root/red_tuberia/red-distribucion/red-distribucion.component';
-import { ConexionDomiciliariaComponent } from './componente-container/componente-root/red_tuberia/conexion-domiciliaria/conexion-domiciliaria.component';
 
 
 @NgModule({
-  declarations: [ComponenteContainerComponent, ComponenteRootComponent, CaptacionComponent, ReservorioComponent, TecnologiaExcretaComponent, RedTuberiaComponent, LineaConduccionComponent, LineaAduccionComponent, RedDistribucionComponent, ConexionDomiciliariaComponent],
+  declarations: [ComponenteContainerComponent, ComponenteRootComponent, CaptacionComponent, ReservorioComponent, TecnologiaExcretaComponent, LineaConduccionComponent, LineaAduccionComponent, RedDistribucionComponent, ConexionDomiciliariaComponent, RedTuberiaComponent],
   imports: [
     CommonModule,
     ProyectoComponenteRoutingModule
